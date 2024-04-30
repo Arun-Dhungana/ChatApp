183230adbb37d477d92cc1d4dcc749024ad6301c
Arun Dhungana <a.d.since03@gmail.com>
2024-04-30 13:58:07 +0545

    basic changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-extrabold text-blue-500 mb-1"></h1>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mb-1 text-6xl font-extrabold text-blue-500">WhattyApp</h1>
       <p className="mb-10">Live chat app.</p>
-      <Button as={Link} href='/chat'> Start Chatting</Button>
+      <Button as={Link} href="/chat">
+        Start Chatting
+      </Button>
     </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/chat(.*)", "/api/get-token"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-  matcher: ['/chat(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
