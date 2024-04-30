0a28bc07aada66905b34b36c82ccbfbda3c7b494
Arun Dhungana <a.d.since03@gmail.com>
2024-04-30 13:56:48 +0545

    basic config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      butterutil: "commonjs butterutil",
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
